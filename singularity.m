48cb8c6d289efdfbd183f70642aa554d6c4992bd
Curran Robertson <cjr5480@gmail.com>
2023-08-04 16:49:16 -0600

    Update singularity.m

--- singularity.m
+++ singularity.m
@@ -1,12 +1,200 @@
-function f1 = singularity(c, a, n)
-  % This function takes vector inputs for the coefficients (c), x-values (a), and the exponent (n)
-  % of a singularity function, produces the singularity function, and plots the result.
+# Created by Octave 8.2.0, Fri Aug 04 16:49:00 2023 MDT <curranrobertson@Currans-MacBook-Pro.local>
+# name: a
+# type: matrix
+# rows: 1
+# columns: 3
+ 1 2 3
 
-  pkg load symbolic
-syms x;
-  func = 0;
-  for i = 1:length(c)
-    func = c(i)*(x-a(i))^n(i) + func;
-  end
-disp(func)
-end
+
+# name: ans
+# type: class
+# classname: sym
+# length: 6
+# name: ascii
+# type: cell
+# rows: 1
+# columns: 1
+# name: <cell-element>
+# type: sq_string
+# elements: 1
+# length: 1
+x
+
+
+
+
+
+# name: extra
+# type: cell
+# rows: 1
+# columns: 1
+# name: <cell-element>
+# type: matrix
+# rows: 0
+# columns: 0
+
+
+
+
+
+# name: flat
+# type: cell
+# rows: 1
+# columns: 1
+# name: <cell-element>
+# type: sq_string
+# elements: 1
+# length: 1
+x
+
+
+
+
+
+# name: pickle
+# type: cell
+# rows: 1
+# columns: 1
+# name: <cell-element>
+# type: sq_string
+# elements: 1
+# length: 11
+Symbol('x')
+
+
+
+
+
+# name: size
+# type: cell
+# rows: 1
+# columns: 1
+# name: <cell-element>
+# type: matrix
+# rows: 1
+# columns: 2
+ 1 1
+
+
+
+
+
+# name: unicode
+# type: cell
+# rows: 1
+# columns: 1
+# name: <cell-element>
+# type: sq_string
+# elements: 1
+# length: 1
+x
+
+
+
+
+
+
+
+# name: c
+# type: matrix
+# rows: 1
+# columns: 3
+ 1 2 3
+
+
+# name: n
+# type: matrix
+# rows: 1
+# columns: 3
+ 1 2 3
+
+
+# name: x
+# type: class
+# classname: sym
+# length: 6
+# name: ascii
+# type: cell
+# rows: 1
+# columns: 1
+# name: <cell-element>
+# type: sq_string
+# elements: 1
+# length: 1
+x
+
+
+
+
+
+# name: extra
+# type: cell
+# rows: 1
+# columns: 1
+# name: <cell-element>
+# type: matrix
+# rows: 0
+# columns: 0
+
+
+
+
+
+# name: flat
+# type: cell
+# rows: 1
+# columns: 1
+# name: <cell-element>
+# type: sq_string
+# elements: 1
+# length: 1
+x
+
+
+
+
+
+# name: pickle
+# type: cell
+# rows: 1
+# columns: 1
+# name: <cell-element>
+# type: sq_string
+# elements: 1
+# length: 11
+Symbol('x')
+
+
+
+
+
+# name: size
+# type: cell
+# rows: 1
+# columns: 1
+# name: <cell-element>
+# type: matrix
+# rows: 1
+# columns: 2
+ 1 1
+
+
+
+
+
+# name: unicode
+# type: cell
+# rows: 1
+# columns: 1
+# name: <cell-element>
+# type: sq_string
+# elements: 1
+# length: 1
+x
+
+
+
+
+
+
+
