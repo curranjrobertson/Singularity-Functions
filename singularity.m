8cb26e85abaa4609ebbc2683d4409eeb0946e122
Curran Robertson <cjr5480@gmail.com>
2023-08-05 01:32:08 -0600

    Update singularity.m

--- singularity.m
+++ singularity.m
@@ -1,10 +1,11 @@
 function f1 = singularity(c, a, n)
+  % Author: Curran Robertson
+  % Date: August 5, 2023
   % This function takes vector inputs for the coefficients (c), x-values (a), and the exponent (n)
   % of a singularity function, produces the singularity function, and plots the result.
 
   pkg load symbolic
   syms x;
-  func = 0;
   for i = 1:length(c)
     func = c(i)*(x-a(i))^n(i).*(heaviside(x-a(i)))
     if (i - 1) <= 0
@@ -16,12 +17,11 @@ function f1 = singularity(c, a, n)
     end
   end
 
+  y = 0;
   for i = 1:length(c)
-    y = piecewise(cond(i), val(i))
-    fplot(y)
-    hold on
+    y = piecewise(cond(i), val(i)) + y
   end
 
-  hold off
+  ezplot(y)
   disp(func) % Replace Parentheses with angle brackets
 end
