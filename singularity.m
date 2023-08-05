0efd12aea43f56d6f4a6943643dce5108f9b6bf5
Curran Robertson <cjr5480@gmail.com>
2023-08-05 01:09:35 -0600

    Update singularity.m

--- singularity.m
+++ singularity.m
@@ -3,19 +3,25 @@ function f1 = singularity(c, a, n)
   % of a singularity function, produces the singularity function, and plots the result.
 
   pkg load symbolic
-syms x;
+  syms x;
   func = 0;
   for i = 1:length(c)
-    func = c(i)*(x-a(i))^n(i).*(heaviside(x-a(i))) + func
+    func = c(i)*(x-a(i))^n(i).*(heaviside(x-a(i)))
     if (i - 1) <= 0
-      figure
-      ezplot(func, [0 a(i)])
+      val(i) = func
+      cond(i) = ( 0 < x ) & (x < a(i) )
     else
-      figure
-      ezplot(func, [a(i-1) a(i)])
+      val(i) = func
+      cond(i) = ( a(i-1) < x ) & (x < a(i) )
     end
   end
-hold off
 
-disp(func) % Replace Parentheses with angle brackets
+  for i = 1:length(c)
+    y = piecewise(cond(i), val(i))
+    fplot(y)
+    hold on
+  end
+
+  hold off
+  disp(func) % Replace Parentheses with angle brackets
 end
