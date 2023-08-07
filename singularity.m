3f5970648b1d5fb7b6d157ee23b219001f6a5050
Curran Robertson <cjr5480@gmail.com>
2023-08-07 13:04:16 -0600

    Update singularity.m

--- singularity.m
+++ singularity.m
@@ -11,17 +11,17 @@ function f1 = singularity(c, a, n)
   L = length(c)
  % Error caused by not containing all x-values
   for i = 1:L
-    func1 = c(i)*(x-a(i))^n(i)%*(heaviside(x-a(i)))
+    func = c(i)*(x-a(i))^n(i)*(heaviside(x-a(i)))
     if (i - 1) <= 0
-      y = func1
-      cond1 = ( 0 < x)
+      val(i) = func
+      cond(i) = ( 0 < x)
     else
-      y = func1
-      cond1 = ( 0 < x)
+      val(i) = func
+      cond(i) = ( 0 < x)
     end
     cond2 = x <= 0
     func2 = 0
-    y = piecewise(cond1, func1, cond2, func2)
+    y = piecewise(cond(i), val(i), cond2, func2)
     ezplot(y)
   end
   %disp(func) % Replace Parentheses with angle brackets
