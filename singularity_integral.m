8f60cb8180b86ccc9f49b371761a743fca508a6e
Curran Robertson <cjr5480@gmail.com>
2023-08-09 11:22:38 -0600

    Update singularity_integral.m

--- singularity_integral.m
+++ singularity_integral.m
@@ -4,7 +4,9 @@ function singularity_integral(c, a, n, t)
   for i = 1:t
     n(:) = n(:) + 1;
   end
+  for i = 1:length(c)
+    y = y + c(i)*(x-a(i))^(n(i))*heaviside(x-a(i));
+  end
 
-  y = y + c(i)*(x-a(i))^(n(i))*heaviside(x-a(i));
   disp(y)
 end
