4d242315d8f0f56f0f473dbe7f6c7a40cf3d2684
Curran Robertson <cjr5480@gmail.com>
2023-08-08 23:24:42 -0600

    update to new file

---
+++ plot_singularity_functions.m
@@ -0,0 +1,19 @@
+function plot_singularity_functions(c, a, n)
+  % Author: Curran Robertson
+  % Date: August 5, 2023 (W.I.P)
+  % This function takes vector inputs for the coefficients (c), x-values (a), and the exponent (n)
+  % of a singularity function, produces the singularity function, and plots the result.
+
+    x = linspace(min(a), max(a), 1000);
+    y = zeros(size(x));
+
+    for i = 1:length(c)
+        y = y + c(i) * (x - a(i)).^n(i);
+    end
+
+    plot(x, y);
+    xlabel('x');
+    ylabel('Singularity Function');
+    title('Plot of Singularity Functions');
+
+end
