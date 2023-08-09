35223a0ad935d620ba9afe44ed7cc254849faddd
Curran Robertson <cjr5480@gmail.com>
2023-08-09 10:37:03 -0600

    add heaviside function

--- plot_singularity_functions.m
+++ plot_singularity_functions.m
@@ -1,12 +1,16 @@
 function plot_singularity_functions(c, a, n)
   % This function takes vector inputs for the coefficients (c), x-values (a), and the exponent (n)
   % of a singularity function, produces the singularity function, and plots the result.
-
+    pkg load symbolic
     x = linspace(min(a), max(a), 1000);
     y = zeros(size(x));
 
     for i = 1:length(c)
-        y = y + c(i) * (x - a(i)).^n(i);
+      if n(i) >= 0
+        y = y + c(i).*(x-a(i)).^(n(i)).*heaviside(x-a(i));
+      else
+        y = y + c(i).*(x - a(i)).^n(i);
+      end
     end
 
     plot(x, y);
