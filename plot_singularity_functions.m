6a5d921ae85ef78ef1c84797cfc2f1365e7a5f12
Curran Robertson <cjr5480@gmail.com>
2023-08-09 11:38:02 -0600

    add integration constants

--- plot_singularity_functions.m
+++ plot_singularity_functions.m
@@ -1,6 +1,7 @@
 function plot_singularity_functions(c, a, n)
+  % Curran Robertson
   % This function takes vector inputs for the coefficients (c), x-values (a), and the exponent (n)
-  % of a singularity function, produces the singularity function, and plots the result.
+  % of a singularity function and plots the result.
     pkg load symbolic
     x = linspace(min(a), max(a), 1000);
     y = zeros(size(x));
@@ -8,7 +9,6 @@ function plot_singularity_functions(c, a, n)
     for i = 1:length(c)
         y = y + c(i).*(x-a(i)).^(n(i)).*heaviside(x-a(i));
     end
-    disp(y)
     plot(x, y);
     xlabel('x');
     ylabel('Singularity Function');
--- singularity_integral.m
+++ singularity_integral.m
@@ -1,11 +1,20 @@
 function singularity_integral(c, a, n, t)
+  % Curran Robertson
+  % This function takes vector inputs for the coefficients (c), x-values (a), the exponent (n), and the number of integrations
+  % of a singularity function and produces the singularity function.
   pkg load symbolic
-  syms x y
+  syms x y d e
+  const = 0; % initiate constant of integration
   for i = 1:t
     n(:) = n(:) + 1;
+    if i == 3
+      const = d;
+    elseif i == 4
+      const = d*x + e;
+    end
   end
   for i = 1:length(c)
-    y = y + c(i)*(x-a(i))^(n(i))*heaviside(x-a(i));
+    y = y + c(i)*(x-a(i))^(n(i))*heaviside(x-a(i)) + const;
   end
 
   disp(y)
